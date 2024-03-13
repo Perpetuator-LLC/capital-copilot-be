f3b80ac96297eb686230ac2e7dfcc7e20bbdb635
Nik <nik.cimino@gmail.com>
2024-03-13 12:32:52 -0600

    feat: limit admin access to IP addresses in ADMIN_ALLOWED_IPS

---
+++ users/middleware.py
@@ -0,0 +1,41 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) 2024 eContriver LLC
+#  This file is part of Capital Copilot from eContriver.
+#  -
+#  Capital Copilot from eContriver is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  any later version.
+#  -
+#  Capital Copilot from eContriver is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  -
+#  You should have received a copy of the GNU General Public License
+#  along with Capital Copilot from eContriver.  If not, see <https://www.gnu.org/licenses/>.
+# ------------------------------------------------------------------------------
+
+import os
+
+from django.http import HttpResponseForbidden
+
+
+def is_valid_ip(ip_addr, allowed_ips):
+    # Consider supporting ranges, subnets, etc. (see package: `ipaddress`)
+    return ip_addr in allowed_ips
+
+
+class RestrictAdminMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        allowed_ips = os.getenv("ADMIN_ALLOWED_IPS", "").split(",")
+        if request.path.startswith("/admin/"):
+            ip_addr = request.META.get("REMOTE_ADDR")
+            if not is_valid_ip(ip_addr, allowed_ips):
+                return HttpResponseForbidden("Access Denied")
+
+        response = self.get_response(request)
+        return response
